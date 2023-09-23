8c3a63ea2c058ac759de3a53cdf9f263192289d8
Irene <irene.muthomi@student.moringaschool.com>
2023-09-24 00:06:51 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
-// Write your code here
+ let num1= 2;
+ let num2= 31;
+ 
+ const multiply = num1*num2;
+
+
+let random = Math.random().toFixed(1) *10
+
+let num3 = 25
+let num4 = 7
+
+let mod = num3 % num4
+
+let max = Math.max(1,3,6,7,9,13,16,19,20)
